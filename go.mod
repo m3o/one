0efe8628f2541c480d99f322fc7d136cbe3e9c78
Asim Aslam <asim@aslam.me>
2021-05-18 17:30:14 +0100

    add logging handler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/m3o/one
 
 go 1.15
 
-require github.com/micro/services/url/proxy v0.0.0-20210518160925-bb892b5f01b1
+require (
+	github.com/gorilla/handlers v1.5.1
+	github.com/micro/services/url/proxy v0.0.0-20210518160925-bb892b5f01b1
+)
--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/micro/services/url/proxy"
 )
 
 func main() {
+	// logging handler
+	handler := handlers.LoggingHandler(os.Stdout, http.HandlerFunc(proxy.Handler))
 	// register the proxy handler
-	http.HandleFunc("/", proxy.Handler)
+	http.Handle("/", handler)
 	// run on port 8080
 	http.ListenAndServe(":8080", nil)
 }
