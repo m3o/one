6f25431417346867a251c5371b7ea3f996389e9f
Asim Aslam <asim@aslam.me>
2021-12-15 19:43:14 +0000

    add function proxy

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -25,6 +25,9 @@ var (
 
 	// host to proxy for Apps
 	AppHost = "m3o.app"
+
+	// host to proxy for Functions
+	FunctionHost = "m3o.sh"
 )
 
 var (
@@ -32,11 +35,126 @@ var (
 
 	// local cache
 	appMap = map[string]*backend{}
+
+	ftx sync.RWMutex
+
+	functionMap = map[string]*backend{}
 )
 
 type backend struct {
 	url     *url.URL
 	created time.Time
+}
+
+func functionProxy(w http.ResponseWriter, r *http.Request) {
+	// no subdomain
+	if r.Host == FunctionHost {
+		return
+	}
+
+	// lookup the app map
+	ftx.RLock()
+	bk, ok := functionMap[r.Host]
+	ftx.RUnlock()
+
+	// check the url map
+	if ok && time.Since(bk.created) < time.Minute {
+		r.Host = bk.url.Host
+		r.Header.Set("Host", r.Host)
+		httputil.NewSingleHostReverseProxy(bk.url).ServeHTTP(w, r)
+		return
+	}
+
+	subdomain := strings.TrimSuffix(r.Host, "."+FunctionHost)
+
+	// only process one part for now
+	parts := strings.Split(subdomain, ".")
+	if len(parts) > 1 {
+		log.Print("[function/proxy] more parts than expected", parts)
+		return
+	}
+
+	// currently service id is the subdomain
+	id := subdomain
+
+	log.Printf("[function/proxy] resolving host %s to id %s\n", r.Host, id)
+
+	apiURL := APIHost + "/function/proxy"
+
+	// use /v1/
+	if len(APIKey) > 0 {
+		apiURL = APIHost + "/v1/function/proxy"
+	}
+
+	// make new request
+	log.Printf("[function/proxy] Calling: %v", apiURL+"?id="+id)
+	req, err := http.NewRequest("GET", apiURL+"?id="+id, nil)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if req.Header == nil {
+		req.Header = make(http.Header)
+	}
+
+	// set the api key after we're given the header
+	if len(APIKey) > 0 {
+		req.Header.Set("Authorization", "Bearer "+APIKey)
+	}
+
+	// call the backend for the url
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer rsp.Body.Close()
+
+	b, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if rsp.StatusCode != 200 {
+		log.Printf("[function/proxy] Error calling api: status: %v %v", rsp.StatusCode, string(b))
+		http.Error(w, "unexpected error", 500)
+		return
+	}
+
+	result := map[string]interface{}{}
+
+	if err := json.Unmarshal(b, &result); err != nil {
+		log.Print("[function/proxy] failed to unmarshal response")
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// get the destination url
+	u, _ := result["url"].(string)
+	if len(u) == 0 {
+		log.Print("[function/proxy] no response url")
+		return
+	}
+
+	uri, err := url.Parse(u)
+	if err != nil {
+		log.Print("[function/proxy] failed to parse url", err.Error())
+		return
+	}
+
+	ftx.Lock()
+	functionMap[r.Host] = &backend{
+		url:     uri,
+		created: time.Now(),
+	}
+	ftx.Unlock()
+
+	r.Host = uri.Host
+	r.Header.Set("Host", r.Host)
+
+	httputil.NewSingleHostReverseProxy(uri).ServeHTTP(w, r)
 }
 
 func appProxy(w http.ResponseWriter, r *http.Request) {
@@ -257,6 +375,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// m3o.app
 	if strings.HasSuffix(r.Host, AppHost) {
 		appProxy(w, r)
+		return
+	}
+
+	// m3o.sh
+	if strings.HasSuffix(r.Host, FunctionHost) {
+		functionProxy(w, r)
 		return
 	}
 }
